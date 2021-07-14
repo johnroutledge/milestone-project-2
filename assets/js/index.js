3c2c760c85a713e4d770bcf4e3f49c98b3310821
johnroutledge <john_routledge@hotmail.com>
2021-07-14 22:24:31 +0000

    added index.js and some initial JavaScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,33 @@
+ let currentScore = 0;
+ let highScore;
+ let actionWord = ["Pop","Bang","Boing"];
+ let direction = Boolean;
+ let currentCircle = [1,2,3,4,5,6,7,8];
+ let nextCircle;
+
  
+ //credit: Stack Overflow - How to set one minute counter in javascript?
+ function startTimer() {
+    var seconds = 60;
+    function tick() {
+        var clock = document.getElementById("clock");
+        seconds--;
+        clock.innerHTML = (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            alert("Time's Up!");
+        }
+    }
+    tick();
+}
+
+// start the 60-second timer
+startTimer();
+
+// select random action from actionWord array 
+function selectAction() {
+    var action = document.getElementById("action");
+    action.innerHTML = actionWord[Math.floor(Math.random() * actionWord.length)];
+}
+
