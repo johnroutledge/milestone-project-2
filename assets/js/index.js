cc2edfcc4e4bf2cd52ae510b3349aec71c630153
johnroutledge <john_routledge@hotmail.com>
2021-07-15 23:09:51 +0000

    added gameplay javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,26 @@
  let highScore;
  let actionWord = ["Pop","Bang","Boing"];
  let direction = Boolean;
- let currentCircle = [1,2,3,4,5,6,7,8];
+ 
+ let clickedCircle;
  let nextCircle;
 
  
+function startGame() {
+    let clickedCircle;
+    direction = true;
+    currentCircle = 1;
+
+    selectAction();
+    
+    getClickedCircle();
+        
+    calculateCorrectCircle();
+    checkAnswer();
+}
+
  //credit: Stack Overflow - How to set one minute counter in javascript?
- function startTimer() {
+function startTimer() {
     var seconds = 60;
     function tick() {
         var clock = document.getElementById("clock");
@@ -20,10 +34,63 @@
         }
     }
     tick();
+    var action = document.getElementById("action");
+    action.innerHTML = actionWord[Math.floor(Math.random() * actionWord.length)];
 }
 
-// start the 60-second timer
-startTimer();
+//gets the circle player just clicked
+function getClickedCircle () {
+    var circles = document.getElementsByClassName("outer");
+    var circlesCount = circles.length;
+    
+    for (let i=0; i < circlesCount; i++) {
+        circles[i].onclick = function () {
+            console.log(i+1);
+            clickedCircle = i+1;
+        }
+    }
+}
+
+//gets the current circle and current action word to calculate correct circle
+function calculateCorrectCircle() {
+    var step;
+    var currentAction = document.getElementById("action").innerHTML;
+    
+    if (currentAction === "Pop") {
+        step = +1
+    } else if (currentAction === "Bang") {
+        step = -1
+    } else if (currentAction === "Boing") {
+        step = +2
+    }
+
+    if (direction) { 
+        nextCircle = currentCircle + step; 
+    } else { 
+        nextCircle = currentCircle - step;
+    }
+
+    if (nextCircle === "9") {
+        nextCircle = "1"
+    } else if (nextCircle === "10") {
+        nextCircle = "2"
+    } else if (nextCircle === "-1") {
+        nextCircle = "8"
+    } else if (nextCircle === "-2") {
+        nextCircle = "7"
+    }
+    console.log(nextCircle);
+}
+
+function checkAnswer() {
+    console.log("clicked circle=" & clickedCircle);
+    console.log("next circle=" & nextCircle);
+    if (clickedCircle === nextCircle) {
+        console.log("correct");
+    } else {
+        console.log("wrong");
+    }
+}
 
 // select random action from actionWord array 
 function selectAction() {
