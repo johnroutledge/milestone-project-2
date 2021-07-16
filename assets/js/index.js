d3ccba872fe90bda595e37bff5e67893faf35dd2
johnroutledge <john_routledge@hotmail.com>
2021-07-16 22:52:31 +0000

    get gameplay working properly

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,46 @@
  let currentScore = 0;
- let highScore;
+ let highScore = 0;
  let actionWord = ["Pop","Bang","Boing"];
  let direction = Boolean;
- 
  let clickedCircle;
+
  let nextCircle;
 
  
-function startGame() {
-    let clickedCircle;
+function resetGame() {
+    document.getElementById("score").innerHTML = 0;
+    clickedCircle = 0;
     direction = true;
     currentCircle = 1;
-
-    selectAction();
-    
-    getClickedCircle();
-        
-    calculateCorrectCircle();
-    checkAnswer();
+    currentScore = 0;
+    startClock();
+    playGame();
 }
 
- //credit: Stack Overflow - How to set one minute counter in javascript?
-function startTimer() {
+function playGame() {
+    selectAction();
+    calculateCorrectCircle();
+
+    var circles = document.getElementsByClassName("outer");
+    var circlesCount = circles.length;
+    
+    for (let i=0; i < circlesCount; i++) {
+        circles[i].onclick = function () {
+            clickedCircle = (i + 1);
+            console.log("clicked " + clickedCircle);
+            console.log("next " + nextCircle);   
+        
+            checkAnswer();  
+        }
+        
+    }
+
+      
+}
+
+
+ //credit: Stack Overflow - How to set one minute counter in javascript
+function startClock() {
     var seconds = 60;
     function tick() {
         var clock = document.getElementById("clock");
@@ -31,11 +50,11 @@ function startTimer() {
             setTimeout(tick, 1000);
         } else {
             alert("Time's Up!");
+            endGame();
         }
     }
+
     tick();
-    var action = document.getElementById("action");
-    action.innerHTML = actionWord[Math.floor(Math.random() * actionWord.length)];
 }
 
 //gets the circle player just clicked
@@ -45,8 +64,7 @@ function getClickedCircle () {
     
     for (let i=0; i < circlesCount; i++) {
         circles[i].onclick = function () {
-            console.log(i+1);
-            clickedCircle = i+1;
+            clickedCircle = (i + 1);
         }
     }
 }
@@ -55,46 +73,93 @@ function getClickedCircle () {
 function calculateCorrectCircle() {
     var step;
     var currentAction = document.getElementById("action").innerHTML;
-    
+    console.log("CCC " + currentCircle);
     if (currentAction === "Pop") {
-        step = +1
+        step = 1;
     } else if (currentAction === "Bang") {
-        step = -1
+        step = -1;
     } else if (currentAction === "Boing") {
-        step = +2
+        step = 2;
     }
+    
 
     if (direction) { 
-        nextCircle = currentCircle + step; 
+        console.log("direction is true");
+        nextCircle = currentCircle + step;
+        console.log(nextCircle);
     } else { 
+        console.log("direction is false");
         nextCircle = currentCircle - step;
+        console.log(nextCircle);
     }
 
-    if (nextCircle === "9") {
-        nextCircle = "1"
-    } else if (nextCircle === "10") {
-        nextCircle = "2"
-    } else if (nextCircle === "-1") {
-        nextCircle = "8"
-    } else if (nextCircle === "-2") {
-        nextCircle = "7"
+    if (nextCircle === 9) {
+        nextCircle = 1;
+    } else if (nextCircle === 10) {
+        nextCircle = 2;
+    } else if (nextCircle === -1) {
+        nextCircle = 8;
+    } else if (nextCircle === -2) {
+        nextCircle = 7;
+    } else if (nextCircle === 0) {
+        nextCircle = 8;
     }
-    console.log(nextCircle);
+    
+    switch(currentAction) {
+        case "Pop":
+        direction = direction;
+        console.log(direction);
+        break;
+        case "Boing":
+        direction = direction;
+        console.log(direction);
+        break;
+        case "Bang":
+        direction = !direction;
+        console.log(direction);
+        break;
+    }
+
+    currentCircle = nextCircle;
 }
 
 function checkAnswer() {
-    console.log("clicked circle=" & clickedCircle);
-    console.log("next circle=" & nextCircle);
     if (clickedCircle === nextCircle) {
         console.log("correct");
+        updateScore();
+        playGame();
     } else {
         console.log("wrong");
+        endGame();
     }
 }
 
 // select random action from actionWord array 
 function selectAction() {
-    var action = document.getElementById("action");
-    action.innerHTML = actionWord[Math.floor(Math.random() * actionWord.length)];
+    var action = actionWord[Math.floor(Math.random() * actionWord.length)];
+    document.getElementById("action").innerHTML = action;
+    
+    
+    
 }
 
+//enter score update code here
+function updateScore() {
+    currentScore = currentScore + 1;
+    document.getElementById("score").innerHTML = currentScore;
+}
+
+//enter end of game code here
+function endGame() {
+    console.log("game over!");
+    updateHighScore();
+}
+
+function updateHighScore() {
+    highScore = document.getElementById("high-score").innerHTML;
+    
+    if (currentScore > highScore) {
+        //add code to have pop-up high score notification
+        document.getElementById("high-score").innerHTML = currentScore;
+    }
+}
