9b21ed4a41129494f08580c86edb3cac839ca7df
johnroutledge <john_routledge@hotmail.com>
2021-07-30 21:16:07 +0000

    tidy code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
  let currentScore = 0;
  let highScore = 0;
- let actionWord = ["Pop","Bang","Boing"];
+ let actionWord = ['Pop','Bang','Boing'];
  let direction;
  let clickedCircle;
  let nextCircle;
  
 function resetGame() {
-    document.getElementById("score").innerHTML = "Score:00";
-    document.getElementById("action").innerHTML = "Ready?";
+    document.getElementById('score').innerHTML = 'Score:00';
+    document.getElementById('action').innerHTML = 'Ready?';
     clickedCircle = 0;
     direction = true;
     endGame.called = false;
-    currentCircle = 1;
+    currentCircle = 0;
     currentScore = 0;
     // countdown();
 
@@ -19,20 +19,20 @@ function resetGame() {
     let downloadTimer = setInterval(function(){
          if(timeleft <= 0){
             clearInterval(downloadTimer);
-            document.getElementById("action").innerHTML = "GO!";
+            document.getElementById('action').innerHTML = 'Go!';
             startClock();
-            document.getElementById("help-button").classList.add("disabled");
-            document.getElementById("play-button").classList.add("disabled");
-            document.getElementById("demo-button").classList.add("disabled");
+            document.getElementById('help-button').classList.add('disabled');
+            document.getElementById('play-button').classList.add('disabled');
+            // document.getElementById('demo-button').classList.add('disabled');
             disableCircles(true);
             
             //this is a bugfix for audio delay when clicking during game play - see README for more
-            playAudio("silence");
+            playAudio('silence');
             playGame();
         } else if (timeleft === 1) {
-             document.getElementById("action").innerHTML = "Go!";
+             document.getElementById('action').innerHTML = 'Go!';
         } else {
-            document.getElementById("action").innerHTML = (timeleft - 1);
+            document.getElementById('action').innerHTML = (timeleft - 1);
         }
 
 
@@ -40,11 +40,11 @@ function resetGame() {
     }, 1000);
 
     // startClock();
-    // document.getElementById("play-button").classList.add("disabled");
+    // document.getElementById('play-button').classList.add('disabled');
     // disableCircles(true);
     
     // //this is a bugfix for audio delay when clicking during game play - see README for more
-    // playAudio("silence");
+    // playAudio('silence');
     // playGame();
 }
 
@@ -55,12 +55,12 @@ function playGame() {
     selectAction();
     calculateCorrectCircle();
 
-    var circles = document.getElementsByClassName("outer");
+    var circles = document.getElementsByClassName('outer');
     var circlesCount = circles.length;
     
     for (let i=0; i < circlesCount; i++) {
         circles[i].onclick = function () {
-            clickedCircle = (i + 1);
+            clickedCircle = i;
             highlightCurrentCircle();
             checkAnswer();  
         }
@@ -71,19 +71,18 @@ function playGame() {
 function startClock() {
     var seconds = 20;
     function tick() {
-        var clock = document.getElementById("clock");
+        var clock = document.getElementById('clock');
         seconds--;
-        clock.innerHTML = (seconds < 10 ? "0" : "") + String(seconds);
+        clock.innerHTML = (seconds < 10 ? '0' : '') + String(seconds);
         
         if (endGame.called === true) {
              seconds = 0;
-            //  endGame();
         }        
 
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
         } else {
-            playAudio("times_up");
+            playAudio('times_up');
             endGame();
         }
     }
@@ -94,31 +93,29 @@ function startClock() {
 function countdown() {
     var seconds = 4;
     function tick() {
-        var countdown = document.getElementById("action");
+        var countdown = document.getElementById('action');
         seconds--;
         countdown.innerHTML = String(seconds);
 
         if( seconds > 0 ) {
             setTimeout(tick, 1000);
         } else {
-            countdown.innerHTML = "Go!";
+            countdown.innerHTML = 'Go!';
         }
     }
 
     tick();
 }
 
-
-
 //gets the circle player just clicked - currently sitting in playGame function
 // function getClickedCircle () {
-//     var circles = document.getElementsByClassName("outer");
+//     var circles = document.getElementsByClassName('outer');
 //     var circlesCount = circles.length;
     
 //     for (let i=0; i < circlesCount; i++) {
 //         circles[i].onclick = function () {
 //             clickedCircle = (i + 1);
-//             console.log("hey");
+//             console.log('hey');
 //             highlightCurrentCircle();
 //         }
 //     }
@@ -126,30 +123,29 @@ function countdown() {
 
 //highlights the current circle so player knows where they are
 function highlightCurrentCircle() {
-    var circles = document.getElementsByClassName("outer");
+    var circles = document.getElementsByClassName('outer');
     var circlesCount = circles.length;
     
-    playAudio("click");
+    playAudio('click');
 
     for (let i=0; i < circlesCount; i++) {
-        if (i === clickedCircle -1) {
-            document.getElementById(i+1).classList.add("highlighted");
+        if (i === clickedCircle) {
+            document.getElementById(i).classList.add('highlighted');
         } else {
-            document.getElementById(i+1).classList.remove("highlighted");
+            document.getElementById(i).classList.remove('highlighted');
         }
     }
 }
 
-
 //gets the current circle and current action word to calculate correct circle
 function calculateCorrectCircle() {
     var step;
-    var currentAction = document.getElementById("action").innerHTML;
-    if (currentAction === "Pop") {
+    var currentAction = document.getElementById('action').innerHTML;
+    if (currentAction === 'Pop') {
         step = 1;
-    } else if (currentAction === "Bang") {
+    } else if (currentAction === 'Bang') {
         step = -1;
-    } else if (currentAction === "Boing") {
+    } else if (currentAction === 'Boing') {
         step = 2;
     }
     
@@ -160,26 +156,26 @@ function calculateCorrectCircle() {
         nextCircle = currentCircle - step;
     }
 
-    if (nextCircle === 9) {
+    if (nextCircle === 8) {
+        nextCircle = 0;
+    } else if (nextCircle === 9) {
         nextCircle = 1;
-    } else if (nextCircle === 10) {
-        nextCircle = 2;
-    } else if (nextCircle === -1) {
-        nextCircle = 7;
+    } else if (nextCircle === -2) {
+        nextCircle = 6;
     // } else if (nextCircle === -2) {
     //     nextCircle = 7;
-    } else if (nextCircle === 0) {
-        nextCircle = 8;
+    } else if (nextCircle === -1) {
+        nextCircle = 7;
     }
     console.log(nextCircle);
     switch(currentAction) {
-        case "Pop":
+        case 'Pop':
         direction = direction;
         break;
-        case "Boing":
+        case 'Boing':
         direction = direction;
         break;
-        case "Bang":
+        case 'Bang':
         direction = !direction;
         break;
     }
@@ -192,9 +188,9 @@ function checkAnswer() {
         updateScore();
         playGame();
     } else {
-        document.getElementById("action").innerHTML = "Oops!";
-        console.log(document.getElementById("action").innerHTML);
-        playAudio("wrong");
+        document.getElementById('action').innerHTML = 'Oops!';
+        console.log(document.getElementById('action').innerHTML);
+        playAudio('wrong');
         console.log('before');
         wait(1000);  //1 seconds in milliseconds
         console.log('after');
@@ -204,82 +200,82 @@ function checkAnswer() {
 
 // select random action from actionWord array 
 function selectAction() {
-    let actionText = document.getElementById("action");
+    let actionText = document.getElementById('action');
     var action = actionWord[Math.floor(Math.random() * actionWord.length)];
     actionText.innerHTML = action;
     let randomColor = Math.floor(Math.random() * 360) + 1;
-    actionText.style.color = "hsl(" + randomColor + ",100%,54%)";
+    actionText.style.color = 'hsl(' + randomColor + ',100%,54%)';
 }
 
 //enter score update code here
 function updateScore() {
-    var score = document.getElementById("score");
+    var score = document.getElementById('score');
 
     currentScore = currentScore + 1;
-    score.innerHTML = "Score:" + (currentScore < 10 ? "0" : "") + String(currentScore);
+    score.innerHTML = 'Score:' + (currentScore < 10 ? '0' : '') + String(currentScore);
 }
 
 //enter end of game code here
 function endGame() {
     endGame.called = true;
     disableCircles(true);
-    let actionText = document.getElementById("action");
-    actionText.innerHTML = "Game Over";
-    actionText.style.color = "hsl(317,100%,54%)";
-    document.getElementById("help-button").classList.remove("disabled");
-    document.getElementById("play-button").classList.remove("disabled");
-    document.getElementById("demo-button").classList.remove("disabled");
-    document.getElementById("clock").innerHTML = "00";
+    let actionText = document.getElementById('action');
+    actionText.innerHTML = 'Game Over';
+    actionText.style.color = 'hsl(317,100%,54%)';
+    document.getElementById('help-button').classList.remove('disabled');
+    document.getElementById('play-button').classList.remove('disabled');
+    // document.getElementById('demo-button').classList.remove('disabled');
+    document.getElementById('clock').innerHTML = '00';
     resetCircles();
     updateHighScore();
 }
 
 //play selected sound
 function playAudio(soundName) {
-    let sound = document.getElementById("audio");
-    let src = "assets/audio/" + soundName + ".wav"; 
+    let sound = document.getElementById('audio');
+    let src = 'assets/audio/' + soundName + '.wav'; 
     sound.src = src;
     sound.play();
 }
 
 //clears all circles and highlights the top one
 function resetCircles() {
-    var circles = document.getElementsByClassName("outer");
+    var circles = document.getElementsByClassName('outer');
     var circlesCount = circles.length;
    
     for (let i=0; i < circlesCount; i++) {
          if (i===0) {
-            document.getElementById(i+1).classList.add("highlighted");
+            document.getElementById(i).classList.add('highlighted');
          } else {
-            document.getElementById(i+1).classList.remove("highlighted");
+            document.getElementById(i).classList.remove('highlighted');
          }
      } 
 }
 
 function disableCircles(onOff) {
-    var circles = document.getElementsByClassName("outer");
+    var circles = document.getElementsByClassName('outer');
     var circlesCount = circles.length;
    
     for (let i=0; i < circlesCount; i++) {
          if (onOff) {
-            document.getElementById(i+1).classList.add("disabled");
+            document.getElementById(i).classList.add('disabled');
          } else {
-            document.getElementById(i+1).classList.remove("disabled");
+            document.getElementById(i).classList.remove('disabled');
          }
      } 
 }
 
 function updateHighScore() {
-    highScore = document.getElementById("high-score").innerHTML.slice(-2);
+    highScore = document.getElementById('high-score').innerHTML.slice(-2);
     
     if (currentScore > highScore) {
         //add code to have pop-up high score notification
         if (currentScore < 10) {
-            currentScore = "0" + String(currentScore);
+            currentScore = '0' + String(currentScore);
         }
-        document.getElementById("high-score").innerHTML = "High:" + currentScore;
-        playAudio("highscore");
-        document.getElementById("action").innerHTML = "New High!";
+        document.getElementById('high-score').innerHTML = 'High:' + currentScore;
+        playAudio('highscore');
+        document.getElementById('action').innerHTML = 'New High!';
     }
 }
 
@@ -296,7 +292,7 @@ function demoMode() {
     clickedCircle = 0;
     direction = true;
     currentCircle = 1;
-    document.getElementById("score").innerHTML = "Demo mode";
+    document.getElementById('score').innerHTML = 'Demo mode';
     resetCircles();
     
     for (let i=0; i < 8; i++) {        
@@ -313,21 +309,21 @@ function demoMode() {
 }
 
 function highlightCorrectCircle() {
-    var circles = document.getElementsByClassName("outer");
+    var circles = document.getElementsByClassName('outer');
     var circlesCount = circles.length;
     
     for (let i=0; i < circlesCount; i++) {
-        if (i === nextCircle -1) {
-            document.getElementById(i+1).classList.add("highlighted");
+        if (i === nextCircle) {
+            document.getElementById(i).classList.add('highlighted');
         } else {
-            document.getElementById(i+1).classList.remove("highlighted");
+            document.getElementById(i).classList.remove('highlighted');
         }
     }
 }
 
 function pauseDemo() {
-    console.log("Hello");
-    setTimeout(() => {  console.log("World!"); }, 2000);
+    console.log('Hello');
+    setTimeout(() => {  console.log('World!'); }, 2000);
 }
 
 function wait(ms){
